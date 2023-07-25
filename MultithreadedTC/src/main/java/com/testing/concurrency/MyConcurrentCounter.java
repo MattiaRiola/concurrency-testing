583205db4a20104f7c0c2db9fc42db03f4100f8d
MattiaRiola <s280169@studenti.polito.it>
2023-07-25 17:03:40 +0200

    💬 add logging

--- MultithreadedTC/src/main/java/com/testing/concurrency/MyConcurrentCounter.java
+++ MultithreadedTC/src/main/java/com/testing/concurrency/MyConcurrentCounter.java
@@ -3,8 +3,14 @@ package com.testing.concurrency;
 public class MyConcurrentCounter {
     private int count;
     public synchronized void increment() {
+        String threadName = Thread.currentThread().getName();
+        String prefix = "Thread-"+threadName+" : ";
+        System.out.println(prefix + "is running");
+
         int temp = count;
+        System.out.println(prefix + "temp = " + temp);
         count = temp + 1;
+        System.out.println(prefix + "count = " + count);
     }
 
     public int getCount() {
--- MultithreadedTC/src/main/java/com/testing/concurrency/MyCounter.java
+++ MultithreadedTC/src/main/java/com/testing/concurrency/MyCounter.java
@@ -3,8 +3,13 @@ package com.testing.concurrency;
 public class MyCounter {
     private int count;
     public void increment() {
+        String threadName = Thread.currentThread().getName();
+        String prefix = "Thread-"+threadName+" : ";
+        System.out.println(prefix + "is running");
         int temp = count;
+        System.out.println(prefix + "temp = " + temp);
         count = temp + 1;
+        System.out.println(prefix + "count = " + count);
     }
 
     public int getCount() {
